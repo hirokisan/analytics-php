141cc2f91960efde15dd0be718850902917dda22
hirokisan <expsharetoworld@gmail.com>
2017-09-27 10:05:29 +0900

    Enable choose date and other options

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <?php
 
+/**
+ * @error https://www.ja.advertisercommunity.com/t5/%E3%81%9D%E3%81%AE%E4%BB%96-Google-%E3%82%A2%E3%83%8A%E3%83%AA%E3%83%86%E3%82%A3%E3%82%AF%E3%82%B9/403-User-Rate-Limit-Exceeded/td-p/17590#
+ */
+
 // Load the Google API PHP Client Library.
 require_once __DIR__ . '/vendor/autoload.php';
 
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
+const START_DATE = '2017-09-24';
+
+$i = 0;
+$today = date('Y-m-d', strtotime('-1 hour', time()));
+$start_date = date(("Y-m-d"), strtotime(START_DATE));
+$end_date = $start_date;
+
 $analytics = initializeAnalytics();
-$response = getReport($analytics);
-printResults($response);
+
+while($end_date != $today){
+  $response = getReport($analytics, $start_date, $end_date);
+  printResults($response);
+  $i++;
+  $start_date = date(("Y-m-d"), strtotime("+". $i. " day",strtotime(START_DATE)));
+  $end_date = $start_date;
+}
 
 function initializeAnalytics()
 {
@@ -29,26 +46,46 @@ function initializeAnalytics()
   return $analytics;
 }
 
-function getReport($analytics) {
+function getReport($analytics, $start_date, $end_date) {
 
   // Replace with your view ID, for example XXXX.
   $VIEW_ID = $_ENV['VIEW_ID'];
 
   // Create the DateRange object.
   $dateRange = new Google_Service_AnalyticsReporting_DateRange();
-  $dateRange->setStartDate("7daysAgo");
-  $dateRange->setEndDate("today");
+  $dateRange->setStartDate($start_date);
+  $dateRange->setEndDate($end_date);
 
   // Create the Metrics object.
   $sessions = new Google_Service_AnalyticsReporting_Metric();
-  $sessions->setExpression("ga:sessions");
+  $sessions->setExpression("ga:pageviews");
   $sessions->setAlias("sessions");
+
+  $dimention1 = new \Google_Service_AnalyticsReporting_Dimension();
+  $dimention1->setName( 'ga:channelGrouping' );
+
+  $dimention2 = new \Google_Service_AnalyticsReporting_Dimension();
+  $dimention2->setName( 'ga:deviceCategory' );
+
+  $dimention3 = new \Google_Service_AnalyticsReporting_Dimension();
+  $dimention3->setName( 'ga:date' );
+
+  // Create the Filter object.
+  $filter = new \Google_Service_AnalyticsReporting_DimensionFilter();
+  $filter->setDimensionName( 'ga:pagePath' );
+  $filter->setOperator( 'BEGINS_WITH' );
+  $filter->setExpressions( [ '/get-marry-with-vietnamese-in-japan-1/' ] );
+
+  $filters = new \Google_Service_AnalyticsReporting_DimensionFilterClause();
+  $filters->setFilters([$filter]);
 
   // Create the ReportRequest object.
   $request = new Google_Service_AnalyticsReporting_ReportRequest();
   $request->setViewId($VIEW_ID);
   $request->setDateRanges($dateRange);
   $request->setMetrics(array($sessions));
+  $request->setDimensions( [ $dimention1, $dimention2, $dimention3 ] );
+  $request->setDimensionFilterClauses( $filters );
 
   $body = new Google_Service_AnalyticsReporting_GetReportsRequest();
   $body->setReportRequests( array( $request) );
@@ -67,19 +104,32 @@ function printResults($reports) {
       $row = $rows[ $rowIndex ];
       $dimensions = $row->getDimensions();
       $metrics = $row->getMetrics();
+
+	  $results = array();
+
       for ($i = 0; $i < count($dimensionHeaders) && $i < count($dimensions); $i++) {
-        print($dimensionHeaders[$i] . ": " . $dimensions[$i] . "\n");
+        //print($dimensionHeaders[$i] . ": " . $dimensions[$i] . "\n");
+        $results[] = $dimensions[$i];
       }
 
       for ($j = 0; $j < count( $metricHeaders ) && $j < count( $metrics ); $j++) {
         $entry = $metricHeaders[$j];
         $values = $metrics[$j];
-        print("Metric type: " . $entry->getType() . "\n" );
         for ( $valueIndex = 0; $valueIndex < count( $values->getValues() ); $valueIndex++ ) {
           $value = $values->getValues()[ $valueIndex ];
-          print($entry->getName() . ": " . $value . "\n");
+          //print($entry->getName() . ": " . $value . "\n");
+          $results[] = $value;
         }
       }
-    }
+	  setCsv($results);
+	}
   }
+}
+
+function setCsv(array $data){
+	$f = fopen("data.csv", "a");
+	if ( $f ) {
+		fputcsv($f, $data);
+	}
+	fclose($f);
 }
--- readme.md
+++ readme.md
@@ -26,3 +26,7 @@ $ php index.php
 * [Reporting API v4](https://developers.google.com/analytics/devguides/reporting/core/v4/quickstart/service-php)
 * [google/google-api-php-client](https://github.com/google/google-api-php-client)
 * [vlucas/phpdotenv](https://github.com/vlucas/phpdotenv)
+* [Dimensions & Metrics Explorer](https://developers.google.com/analytics/devguides/reporting/core/dimsmets)
+* [メソッド: reports.batchGet](https://developers.google.com/analytics/devguides/reporting/core/v4/rest/v4/reports/batchGet?hl=ja)
+* [PHPを使って GoogleAnalytics にある特定ページの情報を受け取る](https://qiita.com/a_yasui/items/9c6fff66aa92a54c8298)
+* [Google Analyticsのランディングページのデータを取得する](http://masalib.hatenablog.com/entry/2016/10/31/220155#複数のカラムMetric-に対応)
